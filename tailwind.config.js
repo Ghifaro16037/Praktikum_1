17480ab037e059b2c3097c9b99c118963cf37315
Ghifaro16037 <2300016037@webmail.uad.ac.id>
2024-11-04 03:49:45 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./index.html",         // Arahkan ke file HTML utama Anda
+      "./src/**/*.{html,js}",  // Termasuk semua file HTML dan JS dalam folder src
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
